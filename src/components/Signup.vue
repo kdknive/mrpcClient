664c4c401d576a3004054ac80781cf48f49e3f39
nadhifalR <nadhifal01@gmail.com>
2021-05-31 22:02:13 +0700

    Signup

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -2,13 +2,14 @@
     <div class="op fixed left-0 right-0 top-0 bottom-0">
         <div class="login-Container bg-green1 top-0 bottom-0 left-0 right-0 fixed m-auto w-5/6 lg:w-3/6 z-50 inline-block justify-center text-center">
             <img @click="ToggleLogin();" class="cursor-pointer w-8 lg:w-10 ml-auto mr-2 mt-2" src="../assets/closeB.svg">
-            <p class="text-loginB text-4xl my-6 mx-8 lg:text-7xl title">SIGN UP</p>
-            <form class="form relative mx-8 md:mx-16 py-5">
+            <p class="text-loginB text-4xl mt-6 mb-3 mx-8 lg:text-7xl title">SIGN UP</p>
+            <form class="form relative mx-8 md:mx-16 py-3">
                 <input type="text" placeholder="Email" class="signInput bg-green1 w-full rounded-xl py-3 px-4 mt-3 mb-5 mx-auto block text-center text-base md:text-lg xl:text-2xl text-loginB">
+                <input type="text" placeholder="Username" class="signInput bg-green1 w-full rounded-xl py-3 px-4 mt-3 mb-5 mx-auto block text-center text-base md:text-lg xl:text-2xl text-loginB">
                 <input type="password" placeholder="Password" class="signInput bg-green1 w-full rounded-xl py-3 px-4 mt-3 mb-5 mx-auto block text-center text-base md:text-lg xl:text-2xl text-loginB">
                 <button type="submit" id="sign-button" class="signButton bg-loginB text-green1 py-3 px-4 w-full rounded-xl cursor-pointer text-base md:text-lg xl:text-2xl">SIGN UP</button>
         	</form>
-            <p class="text-gray-400 text-2xl my-4">- OR -</p>
+            <p class="text-gray-400 text-2xl my-0">- OR -</p>
             <div class="relative mx-8 md:mx-16 py-5">
                 <button class="googleSignup bg-loginB text-green1 py-3 px-4 w-full rounded-xl cursor-pointer mb-5 text-base md:text-lg xl:text-2xl">LOGIN WITH YOUR GOOGLE ACCOUNT</button>
                 <button @click="ToggleSignup();" class="login bg-loginB text-green1 py-3 px-4 w-full rounded-xl cursor-pointer text-base md:text-lg xl:text-2xl">LOGIN</button>
